75386777d0da49c05f3600e327266f39a5dd5dce
kevinrasscode <kevin.amaya@smail.rasmussen.edu>
2018-05-22 11:20:14 -0700

    initial commit

---
+++ SimpleCalculator/InputConverter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace SimpleCalculator
+{
+    public class InputConverter
+    {
+        public double ConvertInputToNumeric(string argTextInput)
+        {
+            double convertedNumber;
+            if(!double.TryParse(argTextInput, out convertedNumber)) 
+                throw new ArgumentException("Expected a numeric value");
+            return convertedNumber;
+        }
+    }
+}
---
+++ SimpleCalculator/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace SimpleCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            InputConverter inputConverter = new InputConverter();
+            CalculatorEngine calculatorEngine = new CalculatorEngine();
+            Console.WriteLine("Please enter your first number.");
+            double firstNumber = inputConverter.ConvertInputToNumeric(Console.ReadLine());
+            Console.WriteLine("Please enter your second number.");
+            double secondNumber = inputConverter.ConvertInputToNumeric(Console.ReadLine());
+            Console.WriteLine("Types of calculation: " +"add or +, minus or -, multiply or *, divide or /.");
+            var operation = Console.ReadLine();
+
+            double result = calculatorEngine.Calculate(operation, firstNumber, secondNumber);
+            
+            Console.WriteLine("This is your result: " + result);
+
+        }
+    }
+}
